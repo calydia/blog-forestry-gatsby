36ca7dbb671bc767e59a72c7aab99b52fd9b44ca
Sanna Mäkinen <sanna.makinen@gmail.com>
2019-09-08 20:37:08 +0300

    Adds a placeholder link to the footer, some preliminary stylings

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,20 +1,12 @@
-import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 export default () => {
-  // const data = useStaticQuery(
-  //   graphql`
-  //     query MySomeLinks {
-  //       allBlockContentBasic {
-  //         nodes {
-  //           body {
-  //             value
-  //           }
-  //         }
-  //       }
-  //     }
-  //   `
-  // );
-
-  return <footer className="main-footer" />
-}
+  return (
+    <footer className="main-footer">
+      <div className="footer-content">
+        <a href="#">Sanna Mäkinen</a>
+      </div>
+    </footer>
+  );
+};
