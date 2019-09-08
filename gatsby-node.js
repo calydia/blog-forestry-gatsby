784bdc6909dd6e04add33ffd3266acec3808e218
Sanna Mäkinen <sanna.makinen@gmail.com>
2019-09-08 19:20:18 +0300

    Adds a template and functionality for the info content

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,10 @@
-const _ = require('lodash')
-const Promise = require('bluebird')
-const path = require('path')
-const { createFilePath } = require('gatsby-source-filesystem')
+const _ = require('lodash');
+const Promise = require('bluebird');
+const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
   return new Promise((resolve, reject) => {
     resolve(
@@ -21,6 +21,7 @@ exports.createPages = ({ graphql, actions }) => {
                     title
                     type
                     path
+                    category
                   }
                 }
               }
@@ -29,21 +30,25 @@ exports.createPages = ({ graphql, actions }) => {
         `
       ).then(result => {
         if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
+          console.log(result.errors);
+          reject(result.errors);
         }
 
-        // Create blog posts pages.
-        const posts = result.data.allMarkdownRemark.edges
+        // Create pages.
+        const posts = result.data.allMarkdownRemark.edges;
 
         _.each(posts, (post, index) => {
           const previous =
-            index === posts.length - 1 ? null : posts[index + 1].node
-          const next = index === 0 ? null : posts[index - 1].node
+            index === posts.length - 1 ? null : posts[index + 1].node;
+          const next = index === 0 ? null : posts[index - 1].node;
 
           const address = post.node.frontmatter.path
             ? post.node.frontmatter.path
-            : post.node.fields.slug
+            : post.node.fields.slug;
+
+          const category = post.node.frontmatter.category
+            ? post.node.frontmatter.category
+            : [''];
 
           createPage({
             path: address,
@@ -54,23 +59,24 @@ exports.createPages = ({ graphql, actions }) => {
               slug: address,
               previous,
               next,
+              category,
             },
-          })
-        })
+          });
+        });
       })
-    )
-  })
-}
+    );
+  });
+};
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const value = createFilePath({ node, getNode });
     createNodeField({
       name: `slug`,
       node,
       value: node.frontmatter.path,
-    })
+    });
   }
-}
+};
