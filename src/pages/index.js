d3a751e9aaf9b22b4a2d29c98a2df2bb440d970d
Sanna Mäkinen <sanna.makinen@gmail.com>
2020-12-08 21:47:11 +0200

    Whoopsie....

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,17 +35,11 @@ export default ({ data }) => {
             let listingFluid =
               node.node.frontmatter.listing_image.childImageSharp.fluid;
             return (
-              <article key={index} className="post">
+              <a key={index} className="post" href={node.node.frontmatter.path}>
                 <Img fluid={listingFluid} />
                 <div className="post-content">
                   <h2>
-                    <a
-                      key={index}
-                      href={node.node.frontmatter.path}
-                      className="blog-link"
-                    >
-                      {node.node.frontmatter.title}
-                    </a>
+                    {node.node.frontmatter.title}
                   </h2>
                   <span className="blog-info">
                     {moment(node.node.frontmatter.post_date)
@@ -54,7 +48,7 @@ export default ({ data }) => {
                     | {node.node.frontmatter.category}
                   </span>
                 </div>
-              </article>
+              </a>
             );
           })}
         </div>
@@ -65,17 +59,11 @@ export default ({ data }) => {
                 node.node.frontmatter.listing_image.childImageSharp.fluid;
               return (
                 <li key={`list-item${index}`} className="blog-list-item">
-                  <article key={index} className="post">
+                  <a key={index} className="post" href={node.node.frontmatter.path}>
                     <Img fluid={listingFluid} />
                     <div className="post-content">
                       <h2>
-                        <a
-                          key={index}
-                          href={node.node.frontmatter.path}
-                          className="blog-link"
-                        >
-                          {node.node.frontmatter.title}
-                        </a>
+                        {node.node.frontmatter.title}
                       </h2>
                       <span className="blog-info">
                         {moment(node.node.frontmatter.post_date)
@@ -84,7 +72,7 @@ export default ({ data }) => {
                         | {node.node.frontmatter.category}
                       </span>
                     </div>
-                  </article>
+                  </a>
                 </li>
               );
             })}
